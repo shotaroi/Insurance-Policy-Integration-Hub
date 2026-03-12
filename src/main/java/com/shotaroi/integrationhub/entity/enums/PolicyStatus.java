ddff3a29829cde926ba5bcb9b9cf362a912c55af
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-12 02:56:20 +0100

    Create PolicyStatus.java

---
+++ src/main/java/com/shotaroi/integrationhub/entity/enums/PolicyStatus.java
@@ -0,0 +1,11 @@
+package com.shotaroi.integrationhub.entity.enums;
+
+/**
+ * Lifecycle status of an insurance policy.
+ */
+public enum PolicyStatus {
+    PENDING,
+    ACTIVE,
+    CANCELLED,
+    EXPIRED
+}
