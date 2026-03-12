2c6e3ace1d927f030ec0627e117004574d81651f
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-12 02:56:39 +0100

    Create InsurancePolicyIntegrationHubApplication.java

---
+++ src/main/java/com/shotaroi/integrationhub/InsurancePolicyIntegrationHubApplication.java
@@ -0,0 +1,12 @@
+package com.shotaroi.integrationhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePolicyIntegrationHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsurancePolicyIntegrationHubApplication.class, args);
+    }
+}
